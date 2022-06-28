cd58f2d1c452a3ff57b8966b2f6633c4d1c4e4cd
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-06-28 12:23:04 -0600

    agregando funciones

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const EventosRutas = require('./src/routes/eventos.routes');
 const HabitacionRutas = require('./src/routes/habitacion.routes');
 const HotelRutas = require('./src/routes/hotel.routes');
 const ServiciosRutas = require('./src/routes/servicios.routes');
+const ReservacionRutas = require('./src/routes/reservacion.routes');
 // MIDDLEWARES -> INTERMEDIARIOS
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -16,7 +17,7 @@ app.use(express.json());
 // CABECERAS
 app.use(cors());
 
-app.use('/api', UsuariosRutas, EventosRutas, HotelRutas, HabitacionRutas, ServiciosRutas);
+app.use('/api', UsuariosRutas, EventosRutas, HotelRutas, HabitacionRutas, ServiciosRutas, ReservacionRutas);
 
 
 module.exports = app;
