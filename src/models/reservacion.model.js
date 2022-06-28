1535ea0761b7b5241b24210e419c57fd816c46b2
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-06-28 12:24:11 -0600

    agregando funciones

---
+++ src/models/reservacion.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+var Schema=mongoose.Schema;
+
+var reservacionesSchema = Schema({
+    idHotel: { type: Schema.Types.ObjectId, ref: 'Hoteles'},
+    idUsuario: { type: Schema.Types.ObjectId, ref: 'Usuarios'},
+    idHabitacion: { type: Schema.Types.ObjectId, ref: 'Habitacion'},
+    fechaInicio:String,
+    fechaFinal:String,
+
+})
+
+module.exports=mongoose.model('reservaciones',reservacionesSchema)
